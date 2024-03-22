6ea49183379b38d797b7feda0b2183e6142cbb31
Alin Stanciu <alinstanciu2011@yahoo.com>
2024-03-22 09:49:26 +0200

    add tests

--- LabAssiAsseProjectV01/src/Main.java
+++ LabAssiAsseProjectV01/src/Main.java
@@ -8,6 +8,9 @@ import validation.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Disable to skip tests.
+        doTests();
+
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
@@ -24,5 +27,66 @@ public class Main {
         // de avut un check: daca profesorul introduce sau nu saptamana la timp
         // daca se introduce nota la timp, se preia saptamana din sistem
         // altfel, se introduce de la tastatura
+    }
+
+    static void doTests() {
+        testAddStudentWithGreaterId();
+        testAddStudentWithSameId();
+    }
+
+    static void testAddStudentWithGreaterId() {
+        // Arrange
+        Validator<Student> studentValidator = new StudentValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        var stud1 = new Student("100", "stud1", 936);
+        studentValidator.validate(stud1);
+        fileRepository1.save(stud1);
+
+        // Act
+        var stud2 = new Student("101", "stud1", 936);
+        studentValidator.validate(stud2);
+        fileRepository1.save(stud2);
+
+        boolean success = true;
+        try {
+            fileRepository1.save(stud1);
+        } catch (Exception ex) {
+            success = false;
+        }
+
+        // Assert
+        if (success)
+            System.out.println("testAddStudentWithGreaterId passed");
+        else
+            System.out.println("testAddStudentWithGreaterId failed");
+    }
+
+    static void testAddStudentWithSameId() {
+        // Arrange
+        Validator<Student> studentValidator = new StudentValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        var stud1 = new Student("100", "stud1", 936);
+        studentValidator.validate(stud1);
+        fileRepository1.save(stud1);
+
+        // Act
+        var stud2 = new Student("100", "stud1", 936);
+        studentValidator.validate(stud2);
+        fileRepository1.save(stud2);
+
+        boolean success = true;
+        try {
+            fileRepository1.save(stud1);
+        } catch (Exception ex) {
+            success = false;
+        }
+
+        // Assert
+        if (success)
+            System.out.println("testAddStudentWithSameId passed");
+        else
+            System.out.println("testAddStudentWithSameId failed");
     }
 }
